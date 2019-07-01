0db67679dbf07c38b7266425a5059115d5c7ba3f
yosoro <yosoro@yosorodeMacBook-Pro.local>
2019-07-02 00:12:38 +0800

    1

--- Summary/AppDelegate.swift
+++ Summary/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     internal func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         touch()
         Bmob.register(withAppKey: "881a726d47210595b04b2f8dbe95b6ef")
+        do{
+            let realm = try Realm()
+        } catch {
+            print("初始化Realm发生错误")
+        }
         // Override point for customization after application launch.
         return true
     }
---
+++ Summary/Date.swift
@@ -0,0 +1,15 @@
+//
+//  Date.swift
+//  Summary
+//
+//  Created by yosoro on 2019/7/1.
+//  Copyright © 2019 yosoro. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Date: Object {
+    @objc dynamic var user:String = ""
+    @objc dynamic var date:Date = Date()
+}
